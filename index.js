9bc779b13c7e4e7230196a44b6c7eb086ce6b03b
정지원 <4dakto@gmail.com>
2017-05-16 22:01:20 +0900

    A Counter Reducer with Test

---
+++ index.js
@@ -0,0 +1,42 @@
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+};
+
+expect(
+    counter(0, { type: 'INCREMENT' })
+).toEqual(1);
+
+expect(
+    counter(1, { type: 'INCREMENT' })
+).toEqual(2);
+
+expect(
+    counter(1, { type: 'INCREMENT' })
+).toEqual(2);
+
+
+expect(
+    counter(2, { type: 'DECREMENT' })
+).toEqual(1);
+
+
+expect(
+    counter(1, { type: 'SOMETHEING_ELSE' })
+).toEqual(1);
+
+expect(
+    counter(undefined, { type: 'SOMETHEING_ELSE' })
+).toEqual(0);
+
+expect(
+    counter(undefined, { })
+).toEqual(0);
+
+console.log('Test passed!');
