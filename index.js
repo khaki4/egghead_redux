bf265dc147497390d84792b4173921f90897705c
정지원 <4dakto@gmail.com>
2017-05-19 22:47:54 +0900

    Redux: Implementing Store from Scratch

--- index.js
+++ index.js
@@ -1,35 +1,50 @@
 const counter = (state = 0, action) => {
-    switch (action.type) {
-        case 'INCREMENT':
-            return state + 1;
-        case 'DECREMENT':
-            return state - 1;
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+const createStore = (reducer) => {
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  };
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+
+    // listener 중복 제거
+    return () => {
+      listeners = listeners.filter(l => {
+          return l !== listener;
+      });
+    };
+  };
+
+  dispatch({});
+
+  return { getState, dispatch, subscribe };
 };
 
-const { createStore } = Redux;
 const store = createStore(counter);
 
-// store has 3 important methods.
-
-// 1> store.getState()
-console.log(store.getState());
-
-// 2> store.dispatch()
-store.dispatch({ type: 'INCREMENT' });
-console.log(store.getState());
-
-// 3> store.subscribe()
 const render = () => {
-    document.body.innerText = store.getState();
+  document.body.innerText = store.getState();
 };
+
 store.subscribe(render);
-// this is for init
 
 window.onload = () => render();
-
 document.addEventListener('click', () => {
-    store.dispatch({ type: 'INCREMENT' });
+  store.dispatch({ type: 'INCREMENT' });
 });
