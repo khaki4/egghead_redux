fda48dfcbad7742cdd1ad913454b17ee7c907a0a
정지원 <4dakto@gmail.com>
2017-05-21 12:50:15 +0900

    Redux: Reducer Composition with Arrays

--- src/index.js
+++ src/index.js
@@ -4,29 +4,66 @@ import ReactDOM from 'react-dom';
 import expect, { createSpy, spyOn, isSpy } from 'expect'
 import deepFreeze from 'deep-freeze';
 
+// https://egghead.io/lessons/javascript-redux-reducer-composition-with-arrays#/tab-transcript
+
+const todo = (state, action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return {
+                id: action.id,
+                text: action.text,
+                completed: false
+            };
+        case 'TOGGLE_TODO':
+            if (state.id !== action.id) {
+                return state;
+            }
+
+            return {
+                ...state,
+                completed: !state.completed
+            };
+        default:
+    }
+};
+
 const todos = (state = [], action) => {
     switch (action.type) {
+        case 'ADD_TODO':
+            console.log('1');
+            return [
+                ...state,
+                todo(undefined, action)
+            ];
         case 'TOGGLE_TODO':
-            return state.map((todo) => {
-                if (todo.id !== action.id) {
-                    return todo;
-                }
-
-
-                /**
-                 * override completed in todo object
-                 * return _extends({}, todo, {
-	                    completed: !todo.completed
-	                });
-                 */
-                return {
-                    ...todo,
-                    completed: !todo.completed,
-                }
-            });
+            console.log('2');
+            return state.map(t => todo(t, action));
         default:
             return state;
     }
+};
+
+const testAddTodo = () => {
+    const stateBefore = [];
+    const action = {
+        type: 'ADD_TODO',
+        id: 0,
+        text: 'Learn Redux'
+    };
+    const stateAfter = [
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        }
+    ];
+
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    expect(
+        todos(stateBefore, action)
+    ).toEqual(stateAfter);
 };
 
 const testToggleTodo = () => {
@@ -42,12 +79,10 @@ const testToggleTodo = () => {
             completed: false
         }
     ];
-
     const action = {
         type: 'TOGGLE_TODO',
         id: 1
     };
-
     const stateAfter = [
         {
             id: 0,
@@ -69,5 +104,7 @@ const testToggleTodo = () => {
     ).toEqual(stateAfter);
 };
 
+
+testAddTodo();
 testToggleTodo();
-console.log('All test passed');
+console.log('All tests passed.');
