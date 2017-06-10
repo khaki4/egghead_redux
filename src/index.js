22bbfe22c633a4444d1ee4b922876f5e8d7d90b6
정지원 <4dakto@gmail.com>
2017-06-10 15:54:35 +0900

    Jiwon :: Redux: Store Methods: getState(), dispatch(), and subscribe()

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
 import expect from 'expect';
 import React from 'react';
-import {createStore} from 'redux';
+import { createStore } from 'redux';
 import ReactDOM from 'react-dom';
 
 const counter = (state = 0, action) => {
     switch (action.type) {
         case 'INCREMENT':
             return state + 1;
-            case 'DECREMENT':
+        case 'DECREMENT':
             return state - 1;
         default:
             return state;
     }
-}
+};
 
-expect(counter(0, {type: 'INCREMENT'})).toEqual(1);
-expect(counter(1, {type: 'INCREMENT'})).toEqual(2);
-expect(counter(2, {type: 'DECREMENT'})).toEqual(1);
-expect(counter(2, {type: 'SOMETHING'})).toEqual(2);
-expect(counter(undefined, {})).toEqual(0);
+const store = createStore(counter);
+const render = () => {
+    document.body.innerHTML = store.getState();
+};
+render();
+store.subscribe(render);
+
+document.addEventListener('click', () => {
+    store.dispatch({ type: 'INCREMENT' });
+});
