4eeac22d99917206ddd5243d14e192322cd5b0c2
정지원 <4dakto@gmail.com>
2017-06-10 17:00:29 +0900

    Jiwon :: Writing a Todo List Reducer (Adding a Todo)

--- src/index.js
+++ src/index.js
@@ -1,45 +1,48 @@
 import expect from 'expect';
 import React from 'react';
-import { createStore } from 'redux';
 import ReactDOM from 'react-dom';
+import deepFreeze from 'deep-freeze';
+import { createStore } from 'redux';
 
-const counter = (state = 0, action) => {
-    switch (action.type) {
-        case 'INCREMENT':
-            return state + 1;
-        case 'DECREMENT':
-            return state - 1;
+const todos = (state = [], action) => {
+    switch (action.type ) {
+        case 'ADD_TODO':
+            return [
+                ...state,
+                {
+                    id: action.id,
+                    text: action.text,
+                    completed: false
+                }
+            ];
         default:
             return state;
     }
 };
 
-const Counter = ({ value, onIncrement, onDecrement }) => {
-    return (
-        <div>
-            <h1>{value}</h1>
-            <button onClick={onIncrement}>+</button>
-            <button onClick={onDecrement}>-</button>
-        </div>
-    );
+const testAddTodo = () => {
+    const stateBefore = [];
+    const action = {
+        type: 'ADD_TODO',
+        id: 0,
+        text: 'Learn Redux'
+    };
+
+    const stateAfter = [
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        }
+    ];
+
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    expect(
+        todos(stateBefore, action)
+    ).toEqual(stateAfter);
+
 };
 
-const store = createStore(counter);
-
-const render = () => {
-    ReactDOM.render(
-        <Counter
-            value={store.getState()}
-            onIncrement={() => store.dispatch({
-                type: 'INCREMENT'
-            })}
-            onDecrement={() => store.dispatch({
-                type: 'DECREMENT'
-            })}
-        />,
-        document.getElementById('root')
-    );
-};
-
-store.subscribe(render);
-render();
+testAddTodo();
