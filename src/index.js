cec9dc52e44a9aafa5c98fa729720390b9aabb1b
정지원 <4dakto@gmail.com>
2017-06-10 17:14:51 +0900

    Jiwon ::  Redux: Writing a Todo List Reducer (Toggling a Todo)

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import deepFreeze from 'deep-freeze';
 import { createStore } from 'redux';
 
 const todos = (state = [], action) => {
-    switch (action.type ) {
+    switch (action.type) {
         case 'ADD_TODO':
             return [
                 ...state,
@@ -15,6 +15,17 @@ const todos = (state = [], action) => {
                     completed: false
                 }
             ];
+        case 'TOGGLE_TODO':
+            return state.map(todo => {
+                if (todo.id !== action.id) {
+                    return todo;
+                }
+
+                return {
+                    ...todo,
+                    completed: !todo.completed
+                }
+            });
         default:
             return state;
     }
@@ -39,10 +50,44 @@ const testAddTodo = () => {
     deepFreeze(stateBefore);
     deepFreeze(action);
 
-    expect(
-        todos(stateBefore, action)
-    ).toEqual(stateAfter);
+    expect(todos(stateBefore, action)).toEqual(stateAfter);
+};
 
+const testToggleTodo = () => {
+    const stateBefore = [
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        },
+        {
+            id: 1,
+            text: 'Go Shopping',
+            completed: false
+        }
+    ];
+    const action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+    };
+    const stateAfter = [
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        },
+        {
+            id: 1,
+            text: 'Go Shopping',
+            completed: true
+        }
+    ];
+
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    expect(todos(stateBefore, action)).toEqual(stateAfter);
 };
 
 testAddTodo();
+testToggleTodo();
