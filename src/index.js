97145bfa96eba5dc7545c26b253113c74e0518a5
정지원 <4dakto@gmail.com>
2017-06-10 20:27:16 +0900

    Jiwon :: Redux: React Todo List Example (Toggling a Todo)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import expect from 'expect';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import deepFreeze from 'deep-freeze';
-import { createStore , combineReducers} from 'redux';
+import { createStore, combineReducers } from 'redux';
 
 const todo = (state, action) => {
   switch (action.type) {
@@ -29,23 +29,15 @@ const todo = (state, action) => {
 const todos = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
-      return [
-        ...state,
-        todo(undefined, action)
-      ];
+      return [...state, todo(undefined, action)];
     case 'TOGGLE_TODO':
-      return state.map(t =>
-        todo(t, action)
-      );
+      return state.map(t => todo(t, action));
     default:
       return state;
   }
 };
 
-const visibilityFilter = (
-  state = 'SHOW_ALL',
-  action
-) => {
+const visibilityFilter = (state = 'SHOW_ALL', action) => {
   switch (action.type) {
     case 'SET_VISIBILITY_FILTER':
       return action.filter;
@@ -64,25 +56,41 @@ class TodoApp extends Component {
   render() {
     return (
       <div>
-        <input ref={node => {
-          this.input = node;
-        }} />
-        <button onClick={() => {
-          store.dispatch({
-            type: 'ADD_TODO',
-            text: this.input.value,
-            id: nextTodoId++
-          });
-          this.input.value = '';
-        }}>
+        <input
+          ref={node => {
+            this.input = node;
+          }}
+        />
+        <button
+          onClick={() => {
+            store.dispatch({
+              type: 'ADD_TODO',
+              text: this.input.value,
+              id: nextTodoId++
+            });
+            this.input.value = '';
+          }}
+        >
           Add Todo
         </button>
         <ul>
-          {this.props.todos.map(todo =>
-            <li key={todo.id}>
+          {this.props.todos.map(todo => (
+            <li
+              key={todo.id}
+              onClick={() => {
+                store.dispatch({
+                  type: 'TOGGLE_TODO',
+                  id: todo.id
+                });
+                this.input.value = '';
+              }}
+              style={{
+                textDecoration: todo.completed ? 'line-through' : 'none'
+              }}
+            >
               {todo.text}
             </li>
-          )}
+          ))}
         </ul>
       </div>
     );
@@ -91,9 +99,7 @@ class TodoApp extends Component {
 
 const render = () => {
   ReactDOM.render(
-    <TodoApp
-      todos={store.getState().todos}
-    />,
+    <TodoApp todos={store.getState().todos} />,
     document.getElementById('root')
   );
 };
