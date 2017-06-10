498c8fa4b062e2e5f6fd144d8a6cccbde4d427f7
정지원 <4dakto@gmail.com>
2017-06-10 16:48:53 +0900

    Jiwon :  Redux: React Counter Example

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import expect from 'expect';
 import React from 'react';
-// import { createStore } from 'redux';
+import { createStore } from 'redux';
 import ReactDOM from 'react-dom';
 
 const counter = (state = 0, action) => {
@@ -14,38 +14,32 @@ const counter = (state = 0, action) => {
     }
 };
 
-const createStore = (reducer) => {
-    let state;
-    let listeners = [];
-
-    const getState = () => state;
-
-    const dispatch = (action) => {
-        state = reducer(state, action);
-        listeners.forEach(listener => listener());
-    };
-
-    const subscribe = (listener) => {
-        listeners.push(listener);
-
-        return () => {
-            listeners = listeners.filter(l => l !== listener);
-        }
-    };
-
-    dispatch({});
-
-    return {
-        getState, subscribe, dispatch
-    };
-}
-const store = createStore(counter);
-const render = () => {
-    document.body.innerHTML = store.getState();
+const Counter = ({ value, onIncrement, onDecrement }) => {
+    return (
+        <div>
+            <h1>{value}</h1>
+            <button onClick={onIncrement}>+</button>
+            <button onClick={onDecrement}>-</button>
+        </div>
+    );
 };
-render();
-store.subscribe(render);
 
-document.addEventListener('click', () => {
-    store.dispatch({ type: 'INCREMENT' });
-});
+const store = createStore(counter);
+
+const render = () => {
+    ReactDOM.render(
+        <Counter
+            value={store.getState()}
+            onIncrement={() => store.dispatch({
+                type: 'INCREMENT'
+            })}
+            onDecrement={() => store.dispatch({
+                type: 'DECREMENT'
+            })}
+        />,
+        document.getElementById('root')
+    );
+};
+
+store.subscribe(render);
+render();
